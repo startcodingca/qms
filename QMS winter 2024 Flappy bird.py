806f81610b6bef17d3fbdeb4ce6ffc900a393e8e
startcodingca <107436735+startcodingca@users.noreply.github.com>
2024-02-14 15:04:59 -0800

    feb14 QMS winter 2024 Flappy bird.py

--- QMS winter 2024 Flappy bird.py
+++ QMS winter 2024 Flappy bird.py
@@ -1,15 +1,24 @@
 #Author: Norman
 #Date Created: Jan 24, 2024
 #QMS - Flappy Bird Game
-#Lesson 1: left off printing event...next lesson close window function
 
 import pygame
 
 print(pygame.init())
+clock = pygame.time.Clock()
                                                                     #(width, height)
 screen = pygame.display.set_mode((500,700))
+
+birdy = 300
 
 gameRunning = True
 while gameRunning == True:
     for event in pygame.event.get():
         print(event)
+
+    birdy = birdy + 10
+
+    screen.fill((204, 255, 255)) #sky blue
+    pygame.draw.rect(screen, (123,234,0), (250,birdy,50,50))
+    pygame.display.update()#render
+    clock.tick(50)
